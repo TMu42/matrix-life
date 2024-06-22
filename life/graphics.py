2279c50063d4d7912829a719741db006dc0fc56b
alpha <11731869+TMu42@users.noreply.github.com>
2024-06-22 15:13:11 +0000

    Graphics now colourizable, this will be achievable with CLI parameters soon. TO-DO: Add CLI options for colours. TO-DO: Leverage PyGame events to handle window operations (and more?). TO-DO: Re-integrate gmain.py and main.py, and leverage PyGame events for terminal mode also (if possible).

--- life/graphics.py
+++ life/graphics.py
@@ -1,5 +1,16 @@
 import os
 import sys
+import numpy
+
+
+ZERO_RED   = 31
+ZERO_GREEN =  1
+ZERO_BLUE  = 46
+
+ONE_RED   =  29
+ONE_GREEN = 206
+ONE_BLUE  = 166
+
 
 with open(os.devnull, 'w') as devnull:
     sys.stdout = devnull
@@ -18,8 +29,6 @@ def initialize(args):
     
     pygame.display.set_caption("Matrix Life")
     
-    print(f"w: {args.resolution_width}; b: {args.resolution_breadth}")
-    
     surface = pygame.display.set_mode((args.resolution_width,
                                        args.resolution_breadth))
     
@@ -27,9 +36,19 @@ def initialize(args):
 
 
 def paint_board(surface, mat):
-    life_grid = pygame.surfarray.make_surface(-mat.T)
+    red_pixels   = (ONE_RED   - ZERO_RED  )*numpy.atleast_3d(mat.T) + ZERO_RED
+    green_pixels = (ONE_GREEN - ZERO_GREEN)*numpy.atleast_3d(mat.T) + ZERO_GREEN
+    blue_pixels  = (ONE_BLUE  - ZERO_BLUE )*numpy.atleast_3d(mat.T) + ZERO_BLUE
     
-    surface.blit(pygame.transform.scale(life_grid, surface.get_rect()[2:]),
-                                                                        (0, 0))
+    life_pixels = numpy.append(
+                        red_pixels, numpy.append(green_pixels,
+                                                 blue_pixels, axis=2), axis=2)
+    
+    #print(f"life_pixels.shape: {life_pixels.shape}")
+    
+    life_surface = pygame.surfarray.make_surface(life_pixels)
+    
+    surface.blit(pygame.transform.scale(life_surface, surface.get_rect()[2:]),
+                 (0, 0))
     
     pygame.display.flip()
