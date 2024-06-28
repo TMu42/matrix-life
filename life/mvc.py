93d0d44c5fd721bc07f675654a381e2e0030d290
alpha <11731869+TMu42@users.noreply.github.com>
2024-06-28 07:04:47 +0000

    Abstract base classes for Model, View and Controller added

---
+++ life/mvc.py
@@ -0,0 +1,41 @@
+
+
+
+class Model:
+    def __init__(self, size, density=None, source=None, offset=None):
+        raise NotImplementedError
+    
+    def step(self, steps=1):
+        raise NotImplementedError
+    
+    def step_to(self, steps):
+        raise NotImplementedError
+    
+    def reset(self):
+        raise NotImplementedError
+
+
+class View:
+    def __init__(self, resolution=None, scale=None, scope=None, colours=None):
+        raise NotImplementedError
+    
+    def move(self, distance):
+        raise NotImplementedError
+    
+    def move_to(self, position):
+        raise NotImplementedError
+    
+    def scale(self, delta):
+        raise NotImplementedError
+    
+    def scale_to(self, value):
+        raise NotImplementedError
+
+
+# Not sure if this abstract base class is needed but might be useful...
+class Controller:
+    def __init__(self, model=None):
+        raise NotImplementedError
+    
+    def connect_model(self, model):
+        raise NotImplementedError
